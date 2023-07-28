a8983a277d87949d07e50e4a6901293218c10938
Black_file <black_file@protonmail.ch>
2023-07-28 17:48:37 +0000

    Add Pro XT Support

--- liquidctl/driver/hydro_platinum.py
+++ liquidctl/driver/hydro_platinum.py
@@ -132,6 +132,14 @@ class HydroPlatinum(UsbHidDriver):
             {'fan_count': 2, 'fan_leds': 0}),
         (0x1b1c, 0x0c37, 'Corsair iCUE H150i Elite RGB (experimental)',
             {'fan_count': 3, 'fan_leds': 0}),
+        (0x1b1c, 0x0c30, 'Corsair Hydro H60i Pro XT',
+            {'fan_count': 2, 'fan_leds': 0}),
+        (0x1b1c, 0x0c2d, 'Corsair Hydro H100i Pro XT',
+            {'fan_count': 2, 'fan_leds': 0}),
+        (0x1b1c, 0x0c2e, 'Corsair Hydro H115i Pro XT',
+            {'fan_count': 2, 'fan_leds': 0}),
+        (0x1b1c, 0x0c2f, 'Corsair Hydro H150i Pro XT',
+            {'fan_count': 3, 'fan_leds': 0}),
     ]
 
     @classmethod
